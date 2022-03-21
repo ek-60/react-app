bc66cb310f0afa3d920e4f0815c5b62958e59765
Eemeli Kyrölä <eemeli.kyrola@gmail.com>
2022-03-21 17:00:53 +0200

    calc
    laskin toimii

--- src/components/WeatherApp/Weather.js
+++ src/components/WeatherApp/Weather.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Weather extends Component {
   render() {
     return (
-      <div>Weather</div>
+      <div>Weather is coming soon..</div>
     )
   }
 }
